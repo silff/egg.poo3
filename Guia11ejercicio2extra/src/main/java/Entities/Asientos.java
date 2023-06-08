df9eeee533927578b9d8e167f4862509dc24072e
silff <128050929+silff@users.noreply.github.com>
2023-06-08 18:26:27 -0300

    8-6

--- Guia11ejercicio2extra/src/main/java/Entities/Asientos.java
+++ Guia11ejercicio2extra/src/main/java/Entities/Asientos.java
@@ -1,13 +1,45 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Entities;
 
-/**
- *
- * @author Silvana
- */
 public class Asientos {
+    
+    private int filas;
+    private String columnas;
+    private boolean ocupado;
+
+    public Asientos() {
+    }
+
+    public Asientos(int filas, String columnas, boolean ocupado) {
+        this.filas = filas;
+        this.columnas = columnas;
+        this.ocupado = ocupado;
+    }
+
+    public int getFilas() {
+        return filas;
+    }
+
+    public void setFilas(int filas) {
+        this.filas = filas;
+    }
+
+    public String getColumnas() {
+        return columnas;
+    }
+
+    public void setColumnas(String columnas) {
+        this.columnas = columnas;
+    }
+
+    public boolean isOcupado() {
+        return ocupado;
+    }
+
+    public void setOcupado(boolean ocupado) {
+        this.ocupado = ocupado;
+    }
+    
     
 }
--- Guia11ejercicio2extra/src/main/java/Entities/Cine.java
+++ Guia11ejercicio2extra/src/main/java/Entities/Cine.java
@@ -1,13 +1,7 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Entities;
 
-/**
- *
- * @author Silvana
- */
 public class Cine {
     
 }
