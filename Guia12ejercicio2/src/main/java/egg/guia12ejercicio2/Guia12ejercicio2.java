3ef2e12870f6cafdd78e18e49295f817d832b8f3
silff <128050929+silff@users.noreply.github.com>
2023-06-14 09:16:23 -0300

    13-6

--- Guia12ejercicio2/src/main/java/egg/guia12ejercicio2/Guia12ejercicio2.java
+++ Guia12ejercicio2/src/main/java/egg/guia12ejercicio2/Guia12ejercicio2.java
@@ -1,16 +1,17 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ 
  */
-
 package egg.guia12ejercicio2;
 
-/**
- *
- * @author Silvana
- */
+import Entities.Electrodomesticos;
+import Enum.Color;
+
 public class Guia12ejercicio2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Electrodomesticos nuevo = new Electrodomesticos();
+        nuevo.setColor(nuevo.getColor());
+        System.out.println(nuevo);
     }
 }
