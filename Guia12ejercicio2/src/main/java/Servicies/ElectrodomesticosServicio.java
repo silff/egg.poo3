be03b6bf5f5575e698dac44be7c5ca3816350679
silff <128050929+silff@users.noreply.github.com>
2023-06-15 00:52:06 -0300

    14-6

--- Guia12ejercicio2/src/main/java/Servicies/ElectrodomesticosServicio.java
+++ Guia12ejercicio2/src/main/java/Servicies/ElectrodomesticosServicio.java
@@ -8,23 +8,23 @@ import java.util.Scanner;
 public class ElectrodomesticosServicio {
 
     private final Scanner sc;
-    private final Electrodomesticos elect;
+    private Electrodomesticos elect;
 
     public ElectrodomesticosServicio() {
         this.sc = new Scanner(System.in).useDelimiter("\n");
-        this.elect = new Electrodomesticos();
     }
 
     /*• Metodo crearElectrodomestico(): le pide la información al usuario y llena el
     electrodoméstico, también llama los métodos para comprobar el color y el consumo. Al
-    precio se le da un valor base de $1000.*/
+    precio se le da un valor base de $1000.(precio, color,
+    //consumo energético (letras entre A y F) y peso)*/
     public void crearElectrodomestico() {
+        elect = new Electrodomesticos();
         double precioBase = 1000;
         System.out.println("precio");
         elect.setPrecio(precioBase + sc.nextDouble());
-
         System.out.println("peso");
-
+        elect.setPeso(sc.nextDouble());
     }
 
     /*• Método precioFinal(): según el consumo energético y su tamaño, aumentará el valor del
@@ -36,13 +36,42 @@ C $600
 D $500
 E $300
 F $100
+     */
+    private void precioConsumo() {
+        switch (elect.getConsumo()) {
+            case A ->
+                elect.setPrecio(elect.getPrecio() + 1000);
+            case B ->
+                elect.setPrecio(elect.getPrecio() + 800);
+            case C ->
+                elect.setPrecio(elect.getPrecio() + 600);
+            case D ->
+                elect.setPrecio(elect.getPrecio() + 500);
+            case E ->
+                elect.setPrecio(elect.getPrecio() + 300);
+            case F ->
+                elect.setPrecio(elect.getPrecio() + 100);
+        }
+    }
 
-PESO PRECIO
+    /*PESO PRECIO
 Entre 1 y 19 kg $100
 Entre 20 y 49 kg $500
 Entre 50 y 79 kg $800
-Mayor que 80 kg $1000
-     */
+Mayor que 80 kg $1000*/
+    private void precioPeso() {
+        double pesoPrecio = 0;
+        pesoPrecio = (elect.getPeso() > 0 && elect.getPeso() < 20) ? pesoPrecio + 100 : pesoPrecio;
+        pesoPrecio = (elect.getPeso() > 19 && elect.getPeso() < 50) ? pesoPrecio + 500 : pesoPrecio;
+        pesoPrecio = (elect.getPeso() > 49 && elect.getPeso() < 80) ? pesoPrecio + 800 : pesoPrecio;
+        pesoPrecio = (elect.getPeso() > 79) ? pesoPrecio + 1000 : pesoPrecio;
+        elect.setPrecio(pesoPrecio + elect.getPrecio());
+    }
+
+    public void precioFinal() {
+        precioConsumo();
+        precioPeso();
+    }
     @Override
     public String toString() {
         return """
@@ -50,3 +79,4 @@ Mayor que 80 kg $1000
     }
 
 }
+// this.color = (color == null) ? Color.DEFAULT : color;
--- Guia12ejercicio2/src/main/java/egg/guia12ejercicio2/Guia12ejercicio2.java
+++ Guia12ejercicio2/src/main/java/egg/guia12ejercicio2/Guia12ejercicio2.java
@@ -3,18 +3,16 @@
  */
 package egg.guia12ejercicio2;
 
-import Entities.Electrodomesticos;
 import Servicies.ElectrodomesticosServicio;
 
 public class Guia12ejercicio2 {
 
     public static void main(String[] args) {
 
-//        ElectrodomesticosServicio nuevo = new ElectrodomesticosServicio();
-//        nuevo.comprobarColor();
-//        nuevo.comprobarConsumoEnergetico();
-//        System.out.println(nuevo);
-        Electrodomesticos a = new Electrodomesticos();
-        System.out.println(a);
+        ElectrodomesticosServicio nuevo = new ElectrodomesticosServicio();
+        nuevo.crearElectrodomestico();
+        nuevo.precioFinal();
+        System.out.println(nuevo);
+
     }
 }
