778ce655253b635785bee13051c215d26587385d
silff <128050929+silff@users.noreply.github.com>
2023-06-18 16:13:52 -0300

    18-6

--- Guia12ejercicio4/src/main/java/egg/guia12ejercicio4/Guia12ejercicio4.java
+++ Guia12ejercicio4/src/main/java/egg/guia12ejercicio4/Guia12ejercicio4.java
@@ -4,14 +4,18 @@
 package egg.guia12ejercicio4;
 
 import entities.Circulo;
+import entities.Rectangulo;
 
 public class Guia12ejercicio4 {
 
     public static void main(String[] args) {
         Circulo c1 = new Circulo(20);
-        System.out.println(c1.area());
+        System.out.println("area " + c1.area());
+        System.out.println("perimetro " + c1.perimetro());
 
-        System.out.println(c1.perimetro());
+        Rectangulo r1 = new Rectangulo(10, 20);
+        System.out.println("area " + r1.area());
+        System.out.println("perimetro " + r1.perimetro());
 
     }
 }
---
+++ Guia12ejercicioExtra1/src/main/java/egg/guia12ejercicioextra1/Guia12ejercicioExtra1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package egg.guia12ejercicioextra1;
+
+/**
+ *
+ * @author Silvana
+ */
+public class Guia12ejercicioExtra1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
