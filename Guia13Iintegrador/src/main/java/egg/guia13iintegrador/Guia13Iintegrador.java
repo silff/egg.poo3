9d71757e6c9e25d3b8e54a0d4015ead1999cbb90
silff <128050929+silff@users.noreply.github.com>
2023-06-28 13:33:54 -0300

    28-6

--- Guia13Iintegrador/src/main/java/egg/guia13iintegrador/Guia13Iintegrador.java
+++ Guia13Iintegrador/src/main/java/egg/guia13iintegrador/Guia13Iintegrador.java
@@ -1,16 +1,12 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+
  */
 
 package egg.guia13iintegrador;
 
-/**
- *
- * @author Silvana
- */
 public class Guia13Iintegrador {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      
     }
 }
--- Guia13Iintegrador/src/main/java/entities/Armadura.java
+++ Guia13Iintegrador/src/main/java/entities/Armadura.java
@@ -1,13 +1,14 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ 
  */
 package entities;
 
-/**
- *
- * @author Silvana
- */
+import Enum.*;
+
 public class Armadura {
+    
+    private Color color;
+    private Propulsores propulsores;
+    private Repulsores repulsores;
     
 }
